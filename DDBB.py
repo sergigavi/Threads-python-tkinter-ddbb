664c40009300ccd9f1bf471a8a187e4adac35674
Sergio García <98185143+sergigavi@users.noreply.github.com>
2022-01-27 22:13:55 +0100

    Code add
    Añado la clase que se ejecuta y la clase de la conexion con la ddbb

---
+++ DDBB.py
@@ -0,0 +1,132 @@
+
+import mysql.connector
+import tkinter
+from tkinter import messagebox
+
+import time
+from time import sleep
+
+#
+
+class DDBB():
+
+    def __init__(self):
+        self.host = "127.0.0.1"
+        self.user = "root"
+        self.passwd = "123abc"
+        #self.conexion = None
+    
+    def conectar(self):
+        self.conexion = mysql.connector.connect(host=self.host, user=self.user, password=self.passwd)
+        #self.cursor = self.conexion.cursor()
+
+        if self.conexion.is_connected():
+            print("conectado correctamente con la base de datos")
+            messagebox.showinfo(message="conectado correctamente con la base de datos")
+            
+
+        self.crearDDBB()
+        
+    def desconectar(self):
+        self.conexion.close()
+        if not self.conexion.is_connected():
+            print("Se ha desconectado la base de datos")
+            messagebox.showinfo(message="Se ha desconectado la base de datos")
+
+    def crearDDBB(self):
+        self.cursor = self.conexion.cursor()
+        self.cursor.execute("CREATE DATABASE IF NOT EXISTS hilos")
+        self.cursor.execute("USE hilos")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS Alumnos("+
+            "dni VARCHAR(9) PRIMARY KEY,"+
+            "nombre VARCHAR(25),"+
+            "apellido1 VARCHAR(25),"+
+            "apellido2 VARCHAR(25),"+
+            "direccion VARCHAR(25),"+
+            "telefono VARCHAR(9) "+
+            ")ENGINE=InnoDB")
+        
+        self.cargarDatos()
+        
+        #self.cursor.close()
+
+    def cargarDatos(self):
+        #self.cursor = self.conexion.cursor() `
+
+        self.cursor.execute("INSERT INTO hilos.Alumnos (dni, nombre, apellido1, apellido2, direccion, telefono) "+
+        "VALUES ('000000001', 'Jorge', 'Carmona', 'Mateo', 'C/Mortadela', '658394853')")
+
+        self.cursor.execute("INSERT INTO hilos.Alumnos (dni, nombre, apellido1, apellido2, direccion, telefono) "+
+        "VALUES ('000000002', 'Manu', 'Gaga', 'Gonzalez', 'C/Lechuga', '685947564')")
+
+        self.cursor.execute("INSERT INTO hilos.Alumnos (dni, nombre, apellido1, apellido2, direccion, telefono) "+
+        "VALUES ('000000003', 'Alex', 'Fernandez', 'de Haro', 'C/Espinaca', '583495040')")
+
+        self.cursor.execute("INSERT INTO hilos.Alumnos (dni, nombre, apellido1, apellido2, direccion, telefono) "+
+        "VALUES ('000000004', 'Luis Alberto', 'Maquina', 'Virtual', 'C/Divas', '863045303')")
+
+        self.cursor.execute("INSERT INTO hilos.Alumnos (dni, nombre, apellido1, apellido2, direccion, telefono) "+
+        "VALUES ('000000005', 'Miguel', 'Sutil', 'Martin', 'C/Datos accesibles', '863045303')")
+
+        self.cursor.execute("INSERT INTO hilos.Alumnos (dni, nombre, apellido1, apellido2, direccion, telefono) "+
+        "VALUES ('000000006', 'Mario', 'Santos', 'Rebollo', 'C/Interfaces', '863045303')")
+
+        self.cursor.execute("INSERT INTO hilos.Alumnos (dni, nombre, apellido1, apellido2, direccion, telefono) "+
+        "VALUES ('000000007', 'Maria', 'Ruiz', 'Carreño', 'C/Bombon', '863045303')")
+
+        self.cursor.execute("INSERT INTO hilos.Alumnos (dni, nombre, apellido1, apellido2, direccion, telefono) "+
+        "VALUES ('000000008', 'Jose Julio', 'Landazuri', 'Elcrack', 'C/Joseju', '863045303')")
+
+        self.cursor.execute("INSERT INTO hilos.Alumnos (dni, nombre, apellido1, apellido2, direccion, telefono) "+
+        "VALUES ('000000009', 'Paula', 'Detodos', 'Losantos', 'C/Paulipaula', '863045303')")
+
+        self.cursor.execute("INSERT INTO hilos.Alumnos (dni, nombre, apellido1, apellido2, direccion, telefono) "+
+        "VALUES ('000000010', 'Bill', 'El', 'Botas', 'C/Ucrania', '863045303')")
+
+        self.conexion.commit()
+
+    def mostrarAlumnos(self, st):
+        self.cursor.execute("SELECT * FROM hilos.Alumnos")
+        st.delete("1.0", tkinter.END)
+        fetchAll = self.cursor.fetchall()
+
+        #st.insert(tkinter.INSERT, fetchAll)
+        #print(fetchAll)
+        
+        #print("Total rows are:  ", len(fetchAll))
+        
+        for fila in fetchAll:
+            st.insert(tkinter.INSERT, ("DNI: "+ fila[0] +"\n") )
+            st.insert(tkinter.INSERT, ("Nombre: "+ fila[1] +"\n") )
+            st.insert(tkinter.INSERT, ("Apellido1: "+ fila[2] +"\n") )
+            st.insert(tkinter.INSERT, ("Apellido2: "+ fila[3] +"\n") )
+            st.insert(tkinter.INSERT, ("Dirección: "+ fila[4] +"\n") )
+            st.insert(tkinter.INSERT, ("Teléfono: "+ fila[5] +"\n") )
+            st.insert(tkinter.INSERT, "\n\n")
+            '''
+            print("DNI: ", fila[0])
+            print("Nombre: ", fila[1])
+            print("Apellido1: ", fila[2])
+            print("Apellido2: ", fila[3])
+            print("Dirección: ", fila[4])
+            print("Teléfono: ", fila[5])
+            print("\n")
+            '''
+
+    def bonanitDDBB(self):
+        self.cursor.execute("DROP SCHEMA IF EXISTS hilos")
+        print("Se ha eliminado la base de datos")
+        print(self.cursor.fetchall())
+
+    def hacerConsulta(self, consulta, st):
+        #self.cursor.execute("SHOW DATABASES")
+        #self.cursor.execute("USE hilos")
+        self.cursor.execute(consulta)
+        st.delete("1.0", tkinter.END)
+        fetchAll = self.cursor.fetchall()
+        st.insert(tkinter.INSERT, fetchAll)
+        print(fetchAll)
+        self.conexion.commit()
+        
+        
+        
---
+++ practicaThreads.py
@@ -0,0 +1,108 @@
+
+from datetime import datetime
+import tkinter as tk
+from tkinter import ttk, messagebox, scrolledtext
+from tkinter import *
+import time
+from time import sleep
+from threading import Thread
+from tkinter import simpledialog
+from DDBB import *
+
+
+class procesos():
+
+    def __init__(self):
+        self.mainVentana = tk.Tk()
+        self.cargarVentana()
+        self.db = DDBB()
+        self.db.conectar()
+        self.generarHilo()
+    
+    def conectarDDBB(self):
+        self.db.conectar()
+        self.generarHilo()
+
+    def generarHilo(self):
+
+        self.hilo = Thread(target=self.mostrarTiempo) #, args=[datetime.now()]
+
+        if self.con2==1:
+            self.hilo.setDaemon(True)
+
+        self.hilo.start()
+
+    def mostrarTiempo(self): #, tiempoInicio      
+        
+        self.threadVivo.set("El hilo está activo")
+
+        if self.con.get() == 0:
+            self.tiempoRemaining.set("50")
+
+            for i in range(int(self.tiempoRemaining.get())):
+                time.sleep(1) #Espera 15 segundos y cierra la conexion
+                self.tiempoRemaining.set(value=str(int(self.tiempoRemaining.get())-1))
+
+            self.db.desconectar()
+            self.threadVivo.set("el hilo NO está activo")
+
+    def consultaSeleccion(self):
+        conSelec = simpledialog.askstring("Consulta de seleccion", "Consulta:")
+        self.db.hacerConsulta(conSelec, self.sT)
+        
+
+    def consultaInsercion(self):
+        conSelec = simpledialog.askstring("Consulta de insercion", "Consulta:")
+        self.db.hacerConsulta(conSelec, self.sT)
+
+    def mostrarAlumnos(self):
+        self.db.mostrarAlumnos(self.sT)
+    
+    def eliminarDDBB(self):
+        self.db.bonanitDDBB()
+        messagebox.showinfo(message="Se ha eliminado la base de datos")
+    
+    def cargarVentana(self):
+
+        self.mainVentana.title("Practica hilos")
+        self.mainVentana.geometry("600x350")
+
+        ttk.Button(self.mainVentana, text="Reconectar DDBB: ", command=self.conectarDDBB).grid(row=0, column=0, padx=10, pady=10, sticky="W")
+        
+        self.con = tk.IntVar()
+        ttk.Checkbutton(self.mainVentana, text="Control cierre", variable=self.con,onvalue=1, offvalue=0).grid(row=0, column=1, padx=10, pady=10, sticky="W")
+
+        self.con2 = tk.IntVar()
+        ttk.Checkbutton(self.mainVentana, text="Dependencia", variable=self.con2, onvalue=1, offvalue=0).grid(row=0, column=2, padx=10, pady=10, sticky="W")
+
+        ttk.Button(self.mainVentana, text="Consulta seleccion: ", command=self.consultaSeleccion).grid(row=1, column=0, padx=10, pady=10, sticky="W")
+
+        ttk.Button(self.mainVentana, text="Consulta insercion: ", command=self.consultaInsercion).grid(row=2, column=0, padx=10, pady=10, sticky="W")
+        #Ejemplo para copiar y probar:
+        #INSERT INTO hilos.Alumnos (dni, nombre, apellido1, apellido2, direccion, telefono) VALUES ('000000011', 'NombreEjemplo', 'ApellidoEjemplo', 'Apellido2Ejemplo', 'C/Ejemplo', '111111111')
+
+        self.threadVivo = tk.StringVar()
+        self.threadVivo.set("El hilo está activo")
+        ttk.Entry(self.mainVentana, textvariable=self.threadVivo, state="readonly").grid(row=1, column=1, padx=10, pady=10, sticky="W")
+
+        self.tiempoRemaining = tk.StringVar()
+        self.tiempoRemaining.set(value="Tiempo de la conexion")
+        ttk.Entry(self.mainVentana, text="Segundos que quedan", textvariable=self.tiempoRemaining, state="readonly").grid(row=2, column=1, padx=10, pady=10, sticky="W")
+
+        ttk.Button(self.mainVentana, text="Mostrar alumnos", command=self.mostrarAlumnos).grid(row=1, column=2, padx=10, pady=10, sticky="W")
+
+        ttk.Button(self.mainVentana, text="Eliminar Base de datos", command=self.eliminarDDBB).grid(row=2, column=2, padx=10, pady=10, sticky="W")
+
+        self.sT = scrolledtext.ScrolledText(self.mainVentana, wrap = tk.WORD, width = 80, height = 10, font = ("Times New Roman",11))
+        self.sT.grid(column = 0,row= 3, pady = 10, columnspan=3, padx = 10)
+
+
+#
+
+
+
+ej = procesos()
+
+
+
+ej.mainVentana.mainloop()
